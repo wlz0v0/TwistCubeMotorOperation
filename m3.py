4e14e2d4a328f1493a4f8eeb4b02a621a5c4e3c8
wlz0v0 <75922528+wlz0v0@users.noreply.github.com>
2021-03-25 21:43:29 +0800

    Update m3.py

--- m3.py
+++ m3.py
@@ -4,19 +4,21 @@ from DRV8825_3 import DRV8825_3
 
 
 class Motor3(DRV8825_3):
-    steps = 66
+    steps = 25
     delay = 0.005
 
     def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
         DRV8825_3.__init__(self, dir_pin, step_pin, enable_pin, mode_pins)
+        self.SetMicroStep('softward', '1/32step')
 
     def motor3_clockwise(self, steps, stepdelay):  # motor3顺时针转动旋转槽
-        self.TurnStep('forward', steps, stepdelay)  # steps的值根据实际情况需要再调
-        self.Stop()
+        self.motor3_anti_clockwise(steps, stepdelay)
+        self.motor3_anti_clockwise(steps, stepdelay)
+        self.motor3_anti_clockwise(steps, stepdelay)
 
     def motor3_anti_clockwise(self, steps, stepdelay):  # motor3顺时针转动旋转槽
         self.TurnStep('backward', steps, stepdelay)  # steps的值根据实际情况需要再调
-        self.Stop()
+        # self.Stop()
 
     def get_cmd(self):
         temp_cmd = input()
@@ -34,6 +36,5 @@ class Motor3(DRV8825_3):
 
 
 if __name__ == "__main__":
-    m3 = Motor3(dir_pin=10, step_pin=9, enable_pin=11, mode_pins=(17, 22, 27))
+    m3 = Motor3(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
     m3.run()
-    
