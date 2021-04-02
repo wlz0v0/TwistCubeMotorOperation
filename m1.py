bf331c50d0d0e2ba7dcd3871b5be29354898b916
wlz0v0 <1228393790@qq.com>
2021-04-02 19:56:20 +0800

    ...

--- m1.py
+++ m1.py
@@ -1,5 +1,7 @@
 import time
 
+import pymongo
+
 from DRV8825_12 import DRV8825_12
 
 
@@ -8,6 +10,13 @@ class Motor1(DRV8825_12):
     both_steps = 66
     only_delay = 0.005
     both_delay = 0.005
+    client = pymongo.MongoClient(
+        "mongodb://crepusculum.xyz:27017/",
+        username="cube",
+        password="123"
+    )
+    cubeDB = client["cube"]
+    mini_cmd_collection = cubeDB["mini_cmd"]
 
     def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
         DRV8825_12.__init__(self, dir_pin, step_pin, enable_pin, mode_pins)
@@ -29,20 +38,41 @@ class Motor1(DRV8825_12):
         # self.Stop()
 
     def get_cmd(self):
-        temp_cmd = input()
+        mini_cmd = self.mini_cmd_collection.find_one()
+        if mini_cmd is None:
+            return
+        if len(mini_cmd["mini_cmd"]) == 0:
+            return "-1"  # 随便返回
+        temp_cmd = mini_cmd["mini_cmd"][0]
         return temp_cmd
+
+    def update(self):
+        mini_cmd = self.mini_cmd_collection.find_one()
+        if mini_cmd is None:
+            return
+        if len(mini_cmd["mini_cmd"]) == 0:
+            return
+        self.mini_cmd_collection.delete_many({})
+        new_mini_cmd = mini_cmd["mini_cmd"]
+        del (new_mini_cmd[0])
+        self.mini_cmd_collection.insert_one({"mini_cmd": new_mini_cmd})
+        return
 
     def run(self):
         while True:
             cmd = self.get_cmd()
             if cmd == "m1_up_only":
                 self.motor1_up_to_3_only(self.only_steps, self.only_delay)
+                self.update()
             elif cmd == "m1_up_both":
                 self.motor1_up_to_3_both(self.both_steps, self.both_delay)
+                self.update()
             elif cmd == "m1_down_only":
                 self.motor1_down_to_2_from_only(self.only_steps, self.only_delay)
+                self.update()
             elif cmd == "m1_down_both":
                 self.motor1_down_to_2_from_both(self.both_steps, self.both_delay)
+                self.update()
             else:
                 time.sleep(1)
 
--- m3.py
+++ m3.py
@@ -1,11 +1,20 @@
 import time
 
+import pymongo
+
 from DRV8825_3 import DRV8825_3
 
 
 class Motor3(DRV8825_3):
     steps = 25
     delay = 0.005
+    client = pymongo.MongoClient(
+        "mongodb://crepusculum.xyz:27017/",
+        username="cube",
+        password="123"
+    )
+    cubeDB = client["cube"]
+    mini_cmd_collection = cubeDB["mini_cmd"]
 
     def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
         DRV8825_3.__init__(self, dir_pin, step_pin, enable_pin, mode_pins)
@@ -21,8 +30,25 @@ class Motor3(DRV8825_3):
         # self.Stop()
 
     def get_cmd(self):
-        temp_cmd = input()
+        mini_cmd = self.mini_cmd_collection.find_one()
+        if mini_cmd is None:
+            return
+        if len(mini_cmd["mini_cmd"]) == 0:
+            return "-1"  # 随便返回
+        temp_cmd = mini_cmd["mini_cmd"][0]
         return temp_cmd
+
+    def update(self):
+        mini_cmd = self.mini_cmd_collection.find_one()
+        if mini_cmd is None:
+            return
+        if len(mini_cmd["mini_cmd"]) == 0:
+            return
+        self.mini_cmd_collection.delete_many({})
+        new_mini_cmd = mini_cmd["mini_cmd"]
+        del (new_mini_cmd[0])
+        self.mini_cmd_collection.insert_one({"mini_cmd": new_mini_cmd})
+        return
 
     def run(self):
         while True:
